8954e0e948a4b5b88bbae143a9cb2fe04fa92522
indahpratiwi7 <pratiwiindh06@gmail.com>
2025-05-21 14:40:07 +0700

    Add files via upload

---
+++ js indah/bab 6/2H.1234javaa.java
@@ -0,0 +1,31 @@
+Public class User {
+    private int jumlahKaki;
+    private string namaHewan;
+    public int getJumlahKaki (){
+        return jumlahKaki;
+    }
+    Public void setJumlahKaki (int jumlahKaki){
+        this.jumlahKaki=jumlahKaki;
+    }
+    Public string getNamaHewan(string namaHewan){
+        return namaHewan;
+    }
+    Public void setNamaHewan(string namaHewan){
+        this.namaHewan=namaHewan;
+    }
+}
+
+public class User {
+    protected string nama;
+    protected string jabatan;
+}
+
+public class Kendaraan {
+    int jumlahKaki;
+    int warna;
+}
+
+public class Hewan {
+    private int jumlahKaki;
+    private string namaHewan;
+}
